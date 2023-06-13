4b2439e2ce00e503dd2172088660de6b3f23f4b1
moisesfdasilva <msesfernandes@gmail.com>
2023-06-13 15:32:53 -0300

    refactor: inserido o comentário javadoc, em Application.java.

--- src/main/java/com/futureh/drone/feeder/Application.java
+++ src/main/java/com/futureh/drone/feeder/Application.java
@@ -3,11 +3,14 @@ package com.futureh.drone.feeder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring main class.
+ */
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
